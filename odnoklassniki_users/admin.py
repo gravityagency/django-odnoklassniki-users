c06087253aa125c52ac08b8c742a2f04c99a964c
ramusus <ramusus@gmail.com>
2014-04-14 16:50:04 +0600

    added admin,  migrations

--- odnoklassniki_users/admin.py
+++ odnoklassniki_users/admin.py
@@ -1,19 +1,19 @@
-# # -*- coding: utf-8 -*-
-# from django.contrib import admin
-# from django.utils.translation import ugettext as _
-# from vkontakte_api.admin import VkontakteModelAdmin
-# from models import User
-#
-# class UserAdmin(VkontakteModelAdmin):
-#     def image_preview(self, obj):
-#         return u'<a href="%s"><img src="%s" height="30" /></a>' % (obj.photo_big, obj.photo)
-#     image_preview.short_description = u'Аватар'
-#     image_preview.allow_tags = True
-#
-#     list_display = ('image_preview','first_name','last_name','vk_link','rate','sex','bdate','timezone','city','country','has_mobile','home_phone','mobile_phone',)
-#     list_display_links = ('first_name','last_name')
-#     list_filter = ('sex','has_mobile','timezone',)
-#     search_fields = ('first_name','last_name','screen_name')
-#     exclude = ('friends_users',)
-#
-# admin.site.register(User, UserAdmin)
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from django.utils.translation import ugettext as _
+from odnoklassniki_api.admin import OdnoklassnikiModelAdmin
+from models import User
+
+class UserAdmin(OdnoklassnikiModelAdmin):
+    def image_preview(self, obj):
+        return u'<a href="%s"><img src="%s" height="30" /></a>' % (obj.pic1024x768, obj.pic50x50)
+    image_preview.short_description = u'Аватар'
+    image_preview.allow_tags = True
+
+    list_display = ('image_preview','name','ok_link','gender','birthday','city','country','has_email',)
+    list_display_links = ('name',)
+    list_filter = ('gender','has_email')
+    search_fields = ('first_name','last_name',)
+#    exclude = ('friends_users',)
+
+admin.site.register(User, UserAdmin)
